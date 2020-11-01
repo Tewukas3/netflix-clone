3723418d22d8a18d0c9c8752cb04a8f3cf54d46a
Jonas <dzonatan3@gmail.com>
2020-11-01 22:51:50 +0000

    Added few basic page components

---
+++ src/pages/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import {
+  FaqsContainer,
+  JumbotronContainer,
+  FooterContainer,
+} from "../containers";
+
+export default function Home() {
+  return (
+    <>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </>
+  );
+}
