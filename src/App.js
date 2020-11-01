d639886be30828d6688bf3cfb9381aa257fd5848
Jonas <dzonatan3@gmail.com>
2020-11-01 18:29:14 +0000

    footer+faqs+optform

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import FooterContainer from "./containers/footer";
+import { FaqsContainer } from "./containers/faqs";
 
 import { JumbotronContainer } from "./containers/jumbotron";
 
 export default function App() {
-  return <JumbotronContainer />;
+  return (
+    <>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </>
+  );
 }
